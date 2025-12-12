27e30d8afa435d2c6bc66371dc008f9c897577fc
simplearyan <aryanphone00620@gmail.com>
2025-12-12 14:44:53 +0530

    Configured Smart URLs 🚀

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,8 +7,12 @@ import tailwind from '@astrojs/tailwind';
 
 import mdx from '@astrojs/mdx';
 
+// Detect environment
+const isGhPages = process.env.GITHUB_ACTIONS === 'true';
+
 export default defineConfig({
   site: 'https://aryan-sharma.github.io', 
-  base: '/',
+  // Vercel uses root '/', GitHub Pages uses '/kinetix'
+  base: isGhPages ? '/kinetix' : '/',
   integrations: [react(), tailwind(), mdx()]
 });
